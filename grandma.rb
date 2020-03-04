949bca7ec4435955b13457b78d3bd69f58973a35
Brandon Marrero <brandonmar97@gmail.com>
2020-03-04 19:24:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,10 @@
 
 def speak_to_grandma(phrase) 
   if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  
     
 end
 # Whatever you say to grandma, she should respond with
